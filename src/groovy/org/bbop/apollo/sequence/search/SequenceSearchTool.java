87b904614eeb1f17adba50aab347a25d3f7b5643
Colin <colin.diesh@gmail.com>
2016-05-24 09:49:00 -0500

    Move sequence search tools classes to plugin

---
+++ src/groovy/org/bbop/apollo/sequence/search/SequenceSearchTool.java
@@ -0,0 +1,13 @@
+package org.bbop.apollo.sequence.search;
+
+
+import java.util.Collection;
+import org.codehaus.groovy.grails.web.json.JSONObject;
+import org.bbop.apollo.BlastAlignment;
+
+public abstract class SequenceSearchTool {
+
+    public abstract void parseConfiguration(JSONObject config) throws SequenceSearchToolException;
+
+    public abstract Collection<BlastAlignment> search(String uniqueToken, String query, String databaseId, StringBuilder t) throws SequenceSearchToolException;
+}
